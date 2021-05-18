b9f4f122ec03e77545cd63870bb0e2249b4b266c
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-17 21:02:36 -0300

    projeto criado

---
+++ src/main/java/com/nelsonenterprises/nelsonspringboot/NelsonspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nelsonenterprises.nelsonspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelsonspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelsonspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelsonenterprises/nelsonspringboot/NelsonspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelsonenterprises.nelsonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelsonspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
