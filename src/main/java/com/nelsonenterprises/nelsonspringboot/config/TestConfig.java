689da1da34ed5a639e8ba4558820efaa1ea45c36
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-19 14:49:53 -0300

    Enum orderstatus and Category entities

--- src/main/java/com/nelsonenterprises/nelsonspringboot/config/TestConfig.java
+++ src/main/java/com/nelsonenterprises/nelsonspringboot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.nelsonenterprises.nelsonspringboot.config;
 
 import com.nelsonenterprises.nelsonspringboot.entities.Order;
 import com.nelsonenterprises.nelsonspringboot.entities.User;
+import com.nelsonenterprises.nelsonspringboot.entities.enums.OrderStatus;
 import com.nelsonenterprises.nelsonspringboot.repositories.OrderRepository;
 import com.nelsonenterprises.nelsonspringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User (null, "Fernardo Sardinha", "fernandosardinha@bol.com", "9531-2001", "treinodebiceps");
         User u2 = new User(null, "Renato Cariani", "renatocariani@maxtitanium.com", "9211-2098", "shapealinhado" );
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
--- src/main/java/com/nelsonenterprises/nelsonspringboot/entities/Order.java
+++ src/main/java/com/nelsonenterprises/nelsonspringboot/entities/Order.java
@@ -3,6 +3,7 @@ package com.nelsonenterprises.nelsonspringboot.entities;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nelsonenterprises.nelsonspringboot.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -21,18 +22,20 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
-    public Order(){
-
+    public Order() {
     }
 
     public Long getId() {
@@ -57,6 +60,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
