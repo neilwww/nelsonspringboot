80051c1ec916236079f9f5f196e59834af495ad8
Mateus Kroeber <mateusksodre@yahoo.com.br>
2021-05-24 21:10:58 -0300

    tratamento de excepetion no metodo update

--- src/main/java/com/nelsonenterprises/nelsonspringboot/services/UserService.java
+++ src/main/java/com/nelsonenterprises/nelsonspringboot/services/UserService.java
@@ -4,11 +4,13 @@ import com.nelsonenterprises.nelsonspringboot.entities.User;
 import com.nelsonenterprises.nelsonspringboot.repositories.UserRepository;
 import com.nelsonenterprises.nelsonspringboot.services.exceptions.DatabaseException;
 import com.nelsonenterprises.nelsonspringboot.services.exceptions.ResourceNotFoundException;
+import org.hibernate.action.internal.EntityActionVetoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,16 +37,20 @@ public class UserService {
         try {
             repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
-            throw new ResourceNotFoundException (id);
-        } catch (DataIntegrityViolationException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getOne(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
